52f8b4ba3cb3edb53456bf75063997fd3e0dcf77
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2022-03-15 18:53:02 +0100

    Update tabla.php

--- tabla.php
+++ tabla.php
@@ -13,7 +13,15 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully <br>";
+    printf("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>");
+printf("<div class='container'>");
+?>
+<br>
+<div class="alert alert-primary" role="alert">
+  <?php echo "Connected successfully <br>";?>
+</div>
+<?php
+
 
 /* mostrar información del host */
 printf("Host info: %s <br>", $conn->host_info);
@@ -22,10 +30,10 @@ printf("Host info: %s <br>", $conn->host_info);
 if ($resultado = $conn->query("SELECT * FROM CLIENTES")) {
     printf("La selección devolvió %d filas.\n", $resultado->num_rows);
     
-    printf("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>");
+
 
     /**TODO: Actividad: Añadir más columnas y cambiar estilo CSS de la tabla Bootstrap*/
-   printf("<table class='table'>");
+   printf("<table class='table table-success table-striped'>");
     /* obtener el array de objetos */
     while ($fila = $resultado->fetch_row()) {
         printf ("<tr><td>%s</td><td>%s</td><td>%s</td> </tr>", $fila[0], $fila[1],$fila[2]);
@@ -33,6 +41,7 @@ if ($resultado = $conn->query("SELECT * FROM CLIENTES")) {
      printf("</table>");
 }
 echo "<br>";
+printf("</div>");
 var_dump($resultado );
 
 /* liberar el conjunto de resultados */
@@ -45,9 +54,6 @@ $conn->close();
 } catch (Exception $e) {
     echo 'Excepción capturada: ',  $e->getMessage(), "\n";
 }
-
-
-
 
 
 
